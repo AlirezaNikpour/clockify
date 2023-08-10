11e106adb14afc7f949319b16b0aefd072d8be33
Alirezanikpour <alirezanikpourstan@gmail.com>
2023-08-10 14:44:24 +0330

    create user setting component

---
+++ src/components/UserSetting/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './UserSetting.module.css'
+function UserSetting() {
+    return (
+        <div className={styles.wrapper}>AN</div>
+    )
+}
+
+export default UserSetting
