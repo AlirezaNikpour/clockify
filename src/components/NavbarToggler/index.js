04c8b9b33eba966b0857e7b0c0c28d82a0c41e55
Alirezanikpour <alirezanikpourstan@gmail.com>
2023-08-10 12:29:21 +0330

    create toggle componet for navbar

---
+++ src/components/NavbarToggler/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './NavbarToggler.module.css'
+function NavbarToggler() {
+    return (
+        <button className={styles.toggle}>
+            <span className={styles.icon}>
+            </span>
+        </button>
+    )
+}
+
+export default NavbarToggler
