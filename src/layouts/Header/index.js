6f096c085d5fb0b97a2bff51545af5a8906f697a
Alirezanikpour <alirezanikpourstan@gmail.com>
2023-08-10 12:29:56 +0330

    create header layout component

--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import styles from './Header.module.css'
+import UserSetting from '../../components/UserSetting'
+import Notification from '../../components/Notification'
+import HelpUser from '../../components/HelpUser'
+import Logo from '../../assets/img/Logo.svg'
+import NavbarToggler from '../../components/NavbarToggler'
 function Header() {
     return (
-        <div>Header</div>
+        <header className={styles.header}>
+            <div className={styles.left}>
+                <NavbarToggler />
+                <div className={styles.logo}>
+                    <img src={Logo} alt='Logo' />
+                </div>
+            </div>
+            <div className={styles.right}>
+                <div>
+                    <HelpUser />
+                </div>
+                <div>
+                    <Notification />
+                </div>
+                <div className={styles[`user-name`]}>
+                    <UserSetting />
+                </div>
+            </div>
+        </header>
     )
 }
 
